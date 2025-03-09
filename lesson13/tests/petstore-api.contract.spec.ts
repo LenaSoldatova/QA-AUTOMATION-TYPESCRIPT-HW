3b582f24d60bb23344bee6cc599d192094cf54e3
Olena Soldatova <lena.soldatova@gmail.com>
2025-03-09 16:47:09 +0200

    fix comments

--- lesson13/tests/petstore-api.contract.spec.ts
+++ lesson13/tests/petstore-api.contract.spec.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import { PactProvider } from '../src/config/pact-provider';
+import { Verifier } from '@pact-foundation/pact';
 import { PetStoreService } from '../src/services/pet-store.service';
+import * as path from 'path';
 
 describe('PactV4 PetsStore tests: Consumer', () => {
     let petStoreService: PetStoreService;
@@ -41,6 +43,21 @@ describe('PactV4 PetsStore tests: Consumer', () => {
                     petStoreService = new PetStoreService(mockServer.url);
                     const responseGet = await petStoreService.getPet(1002);
                     expect(responseGet.data).to.deep.eq(PactProvider.petExample);
+                });
+        });
+    });
+    describe('PetsStore Pact Verification v4: Provider', () => {
+        const providerBaseUrl = 'https://petstore.swagger.io';  // verification URL
+        const pactFilePath = path.resolve(process.cwd(), './pacts/Pets-Web-v4-Pets-API-v4.json'); // contract file
+
+        it('validates the expectations of Matching Service', () => {
+            return new Verifier({
+                providerBaseUrl,
+                pactUrls: [pactFilePath]
+            })
+                .verifyProvider()
+                .then(() => {
+                    console.log('Pact Verification Complete!');
                 });
         });
     });
